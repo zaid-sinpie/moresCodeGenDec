b97ae860146f37be96935be09aed1eb2b5385a1d
zaid-sinpie <zaidmasuldar@gmail.com>
2024-04-03 20:47:28 +0530

    responsive

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({children}){
     return (
-        <section className="flex items-center h-full w-full justify-center gap-4">
+        <section className="flex items-center h-full w-full justify-center gap-4 max-sm:flex-col max-lg:flex-col">
             {children}
         </section>
     )
